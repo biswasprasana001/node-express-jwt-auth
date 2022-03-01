9dfcd2a0ee6180d76c7da19296c5171b82d231ce
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-01 22:41:18 +0530

    I don't know where the bug is, but I have to use this technique to find it

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // handle errors
 const handleErrors = (err) => {
@@ -20,14 +21,23 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "net ninja secret", {
+    expiresIn: maxAge,
+  });
+};
+
 module.exports.signup_get = (req, res) => {
-  res.render("'../views/partials/signup");
+  res.render("signup");
 };
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
@@ -38,5 +48,10 @@ module.exports.login_get = (req, res) => {
 };
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send("user login");
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (error) {
+    res.status(400).json({});
+  }
 };
