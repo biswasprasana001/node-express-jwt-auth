4a1b26e048f93ea8b583ca3916eeb5c4cb4564ae
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-01 15:05:22 +0530

    Will be seeing, where the heck is the bug by comparing the present file after commiting

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,27 @@
 const User = require("../models/User");
 
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // duplicate error code
+  if (err.code === 11000) {
+    errors.email = "that email is aleady registered";
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
-  res.render("singup");
+  res.render("'../views/partials/signup");
 };
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
@@ -9,15 +29,14 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send("error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
-  res.send("new signup");
 };
 module.exports.login_get = (req, res) => {
   res.render("login");
 };
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   res.send("user login");
 };
