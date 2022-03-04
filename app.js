e233e7f22f0d7a6feab5fe5ca9328a9473ac7793
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-04 21:05:15 +0530

    I will checking where the bug is

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
-const cookieParser = require("cookie-parser");
-const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -27,7 +24,6 @@ mongoose
   .catch((err) => console.log(err));
 
 // routes
-app.get("*", checkUser);
-app.get("/", requireAuth, (req, res) => res.render("home"));
-app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
