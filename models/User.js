68b0e640bae805c42d9e51ff0e24d3a5acb9c305
biswasprasana001 <biswasprasana004@gmail.com>
2022-03-05 15:13:36 +0530

    I will be commiting this to see the bug by changin the code and seeing the difference

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -12,8 +13,15 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Please enter a password"],
-    minLength: [6, "Minimum password length is 6 characters"],
+    minlength: [6, "Minimum password length should be 6 characters"],
   },
+});
+
+// fire a function before doc saved to db
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
